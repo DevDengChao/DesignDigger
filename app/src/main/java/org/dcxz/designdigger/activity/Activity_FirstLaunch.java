466dca428162077f02e18e67d69623851cba7ef5
qilvzi <1874518369@qq.com>
2016-12-14 17:16:21 +0800

    对 FirstLaunch 页面做了修改 导入了 Circlelndicator 依赖

--- app/src/main/java/org/dcxz/designdigger/activity/Activity_FirstLaunch.java
+++ app/src/main/java/org/dcxz/designdigger/activity/Activity_FirstLaunch.java
@@ -3,6 +3,7 @@ package org.dcxz.designdigger.activity;
 import android.os.Message;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,7 +12,9 @@ import org.dcxz.designdigger.R;
 import org.dcxz.designdigger.framework.Framework_Activity;
 import org.dcxz.designdigger.util.Util_SP_Manager;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import pers.medusa.circleindicator.widget.CircleIndicator;
 
@@ -25,6 +28,7 @@ import pers.medusa.circleindicator.widget.CircleIndicator;
  */
 
 public class Activity_FirstLaunch extends Framework_Activity {
+    private static final String TAG = "Activity_FirstLaunch";
     /**
      * ViewPager容器
      */
@@ -59,39 +63,70 @@ public class Activity_FirstLaunch extends Framework_Activity {
 
     @Override
     protected void initView() {
+        viewPager = (ViewPager) this.findViewById(R.id.firstLaunch_viewPager);
+        Log.i(TAG, "initView: " + (viewPager == null));
+        circleIndicator = (CircleIndicator) this.findViewById(R.id.indicator);
+        Log.i(TAG, "initView: " + (circleIndicator == null));
 
     }
 
     @Override
     protected void initData() {
         // TODO: 2016/12/13 需要准备素材图像
-
+        viewList = new ArrayList<View>();
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            View view = new View(this);
+            view.setBackgroundColor(0xff000000 | random.nextInt(0x00ffffff));
+            viewList.add(view);
+        }
     }
 
     @Override
     protected void initAdapter() {
         viewPager.setAdapter(new PagerAdapter() {
+
             @Override
-            public int getCount() {
-                return count;
+            public boolean isViewFromObject(View arg0, Object arg1) {
+
+                return arg0 == arg1;
             }
 
             @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view == object;
+            public int getCount() {
+
+                return viewList.size();
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position,
+                                    Object object) {
+                container.removeView(viewList.get(position));
+
+            }
+
+            @Override
+            public int getItemPosition(Object object) {
+
+                return super.getItemPosition(object);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+
+                return "title";
             }
 
             @Override
             public Object instantiateItem(ViewGroup container, int position) {
-                container.addView(content[position]);
-                return content[position];
+                container.addView(viewList.get(position));
+
+                return viewList.get(position);
             }
 
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView(content[position]);
-            }
+
         });
+        circleIndicator.setViewPager(viewPager);
     }
 
     @Override
@@ -104,10 +139,7 @@ public class Activity_FirstLaunch extends Framework_Activity {
 
             @Override
             public void onPageSelected(int position) {
-                for (ImageView iv : indicators) {
-                    iv.setSelected(false);
-                }
-                indicators[position].setSelected(true);
+
                 if ((count - 1) == position) {
                     handler.sendEmptyMessageDelayed(TO_SLASH_ACTIVITY, 2000);
                 }
