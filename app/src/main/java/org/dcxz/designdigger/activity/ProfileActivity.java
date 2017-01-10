c26e388a5cf6e6284e3ae7fff2b89890fef2cdf8
邪恶的黑手 <2325690622@qq.com>
2017-01-10 15:32:27 +0800

    Add ProfileActivity;

---
+++ app/src/main/java/org/dcxz/designdigger/activity/ProfileActivity.java
@@ -0,0 +1,71 @@
+package org.dcxz.designdigger.activity;
+
+import android.os.Message;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.dcxz.designdigger.R;
+import org.dcxz.designdigger.bean.UserInfo;
+import org.dcxz.designdigger.fragment.ProfileFragment;
+import org.dcxz.designdigger.framework.BaseActivity;
+
+import java.io.Serializable;
+
+import butterknife.BindView;
+
+/**
+ * 需要以{@link org.dcxz.designdigger.bean.UserInfo}作为参数,
+ * 并使用{@link #startActivity(Class, Serializable)}启动这个Activity
+ * <br/>
+ * Created by OvO on 2017/1/10.<br/>
+ * ChangeLog :
+ * <pre>
+ * </pre>
+ */
+
+public class ProfileActivity extends BaseActivity {
+
+    @BindView(R.id.activity_profile_toolBar)
+    Toolbar toolbar;
+
+    @Override
+    protected int setContentViewImp() {
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+        return R.layout.activity_profile;
+    }
+
+    @Override
+    protected void initView() {
+        toolbar.setNavigationIcon(R.drawable.toolbar_back);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void initData() {
+        UserInfo user = (UserInfo) getIntent().getSerializableExtra(SERIALIZABLE);
+        ProfileFragment fragment=ProfileFragment.newInstance(user);
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_profile_container,fragment).commit();
+    }
+
+    @Override
+    protected void initAdapter() {
+
+    }
+
+    @Override
+    protected void initListener() {
+        toolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(MainActivity.class);
+                        finish();
+                    }
+                });
+    }
+
+    @Override
+    public void handleMessageImp(Message msg) {
+
+    }
+}
