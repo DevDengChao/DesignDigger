b486d5c6c498b3e7242f292f6ca415d02b49bede
邪恶的黑手 <2325690622@qq.com>
2017-01-15 17:00:59 +0800

    Rename components;

--- app/src/main/java/org/dcxz/designdigger/adapter/ShotHolder.java
+++ app/src/main/java/org/dcxz/designdigger/adapter/ShotHolder.java
@@ -31,43 +31,43 @@ public class ShotHolder extends RecyclerView.ViewHolder {
     /**
      * 头像
      */
-    @BindView(R.id.item_avatar)
+    @BindView(R.id.item_shot_avatar)
     CircleImageView avatar;
     /**
      * 作品
      */
-    @BindView(R.id.item_content)
+    @BindView(R.id.item_shot_content)
     AutoHeightGifImageView content;
     /**
      * Gif控制按键
      */
-    @BindView(R.id.item_gif)
+    @BindView(R.id.item_shot_gif)
     ImageView gif;
 
-    @BindView(R.id.item_rebound)
+    @BindView(R.id.item_shot_rebound)
     TextView rebound;
-    @BindView(R.id.item_attachment)
+    @BindView(R.id.item_shot_attachment)
     TextView attachment;
-    @BindView(R.id.item_view)
+    @BindView(R.id.item_shot_view)
     TextView view;
-    @BindView(R.id.item_comment)
+    @BindView(R.id.item_shot_comment)
     TextView comment;
-    @BindView(R.id.item_like)
+    @BindView(R.id.item_shot_like)
     TextView like;
     /**
      * 用户名
      */
-    @BindView(R.id.item_userName)
+    @BindView(R.id.item_shot_userName)
     TextView userName;
     /**
      * 作品名
      */
-    @BindView(R.id.item_title)
+    @BindView(R.id.item_shot_title)
     TextView title;
     /**
      * 作品创作时间
      */
-    @BindView(R.id.item_time)
+    @BindView(R.id.item_shot_time)
     TextView time;
 
     private String Tag;
@@ -85,9 +85,13 @@ public class ShotHolder extends RecyclerView.ViewHolder {
 
     /**
      * 更新Holder持有的数据
+     * @param shotInfo 待加载的ShotInfo对象
+     * @param imagePath 展示的图像的地址(印象图像质量)
+     * @param activity 用于初始化监听器的活动对象
+     * @param contentClickable 是否允许图像可点击
      */
     @SuppressLint("SetTextI18n")
-    public void update(final ShotInfo shotInfo, String imagePath, final BaseActivity activity) {
+    public void update(final ShotInfo shotInfo, String imagePath, final BaseActivity activity, boolean contentClickable) {
         avatar.setImageResource(R.drawable.progress_rotate);//使用图像占位,避免重用过程中出现图像突变现象
         if (shotInfo.getUser() == null) {//当点击头像进入profileFragment时,服务器返回的信息中不包含user数据
             avatar.setVisibility(View.GONE);
@@ -111,12 +115,14 @@ public class ShotHolder extends RecyclerView.ViewHolder {
             App.imageRequest(imagePath, content, Tag);
             gif.setVisibility(View.INVISIBLE);
         }
-        content.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activity.startActivity(CommentActivity.class, shotInfo);
-            }
-        });
+        if (contentClickable) {
+            content.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    activity.startActivity(CommentActivity.class, shotInfo);
+                }
+            });
+        }// TODO: 2017/1/15 longClickListener
         if (shotInfo.getRebounds_count() == 0) {
             rebound.setVisibility(View.INVISIBLE);
         } else {
@@ -131,7 +137,7 @@ public class ShotHolder extends RecyclerView.ViewHolder {
         }
         view.setText(shotInfo.getViews_count() + "");// 1 -> "1"
         comment.setText(shotInfo.getComments_count() + "");
-        like.setText(shotInfo.getLikes_count() + "");
+        like.setText(shotInfo.getLikes_count() + "");// TODO: 2017/1/15 like
         if (shotInfo.getUser() == null) {//当点击头像进入profileFragment时,服务器返回的信息中不包含user数据
             userName.setVisibility(View.GONE);
         } else {//普通请求包含user数据
