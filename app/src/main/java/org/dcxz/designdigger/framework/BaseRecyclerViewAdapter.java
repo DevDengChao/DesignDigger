023fca27c55112f2833bff1985d3d7dcb015d2d2
邪恶的黑手 <2325690622@qq.com>
2017-01-16 16:25:20 +0800

    Check networkInfo;

--- app/src/main/java/org/dcxz/designdigger/framework/BaseRecyclerViewAdapter.java
+++ app/src/main/java/org/dcxz/designdigger/framework/BaseRecyclerViewAdapter.java
@@ -1,5 +1,8 @@
 package org.dcxz.designdigger.framework;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -23,10 +26,18 @@ public abstract class BaseRecyclerViewAdapter<T> extends RecyclerView.Adapter {
      * 用于提供上下文,获取layoutInflater,设置监听器
      */
     private BaseActivity activity;
+    /**
+     * 当前是否为Wifi网络环境
+     */
+    private boolean isWifiNetwork;
 
     public BaseRecyclerViewAdapter(BaseActivity activity, ArrayList<T> data) {
         this.activity = activity;
         this.data = data;
+        NetworkInfo networkInfo = ((ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+            isWifiNetwork = true;
+        }
     }
 
     public ArrayList<T> getData() {
@@ -42,7 +53,6 @@ public abstract class BaseRecyclerViewAdapter<T> extends RecyclerView.Adapter {
     public void setData(ArrayList<T> data) {
         this.data = data;
         notifyDataSetChanged();
-//        notifyItemRangeChanged(0, data.size());//插入新数据
     }
 
     /**
@@ -71,8 +81,8 @@ public abstract class BaseRecyclerViewAdapter<T> extends RecyclerView.Adapter {
 
     @Override
     public final void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        onBindViewHolderImp(holder, position, data, activity);
+        onBindViewHolderImp(holder, position, data, activity, isWifiNetwork);
     }
 
-    protected abstract void onBindViewHolderImp(RecyclerView.ViewHolder holder, int position, ArrayList<T> data, BaseActivity activity);
+    protected abstract void onBindViewHolderImp(RecyclerView.ViewHolder holder, int position, ArrayList<T> data, BaseActivity activity, boolean isWifiNetwork);
 }
